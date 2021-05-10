c217370876060e09c27e93399db671548afeaa47
eduardoaze <zetoneeduardo@gmail.com>
2021-05-10 12:27:55 -0300

    condição para funcionar

--- jogo.py
+++ jogo.py
@@ -27,6 +27,47 @@ def extrai_naipe (string):
         return '♣'
     else:
         return '♠'
+def possui_movimentos_possiveis (baralho):
+    naipes = []
+    numeros =[]
+    for carta in baralho:
+        if len(carta) == 2:
+            if carta [0] == 'J':
+                naipes.append (carta [1])
+                numeros.append ('11')
+            if carta [0] == 'Q':
+                naipes.append (carta [1])
+                numeros.append ('12')
+            if carta [0] == 'K':
+                naipes.append (carta [1])
+                numeros.append ('13')
+            if carta [0] == 'A':
+                naipes.append (carta [1])
+                numeros.append ('1')
+            if carta[0] != 'A' and carta[0] != 'J' and carta[0] != 'Q' and carta[0] != 'K':
+                naipes.append (carta[1])
+                numeros.append (carta [0])
+        else:
+            naipes.append  (carta [2])
+            numeros.append (10)
+    i = len(baralho) - 1
+    movimentos = 0
+    while i >= 1:
+        if i >= 4:
+            if naipes[i] == naipes [i - 3]:
+                movimentos += 1
+        if numeros [i] == numeros [i - 3]:
+            movimentos += 1
+        else:
+            if naipes [i] == naipes [i - 1]:
+                movimentos += 1
+            if numeros [i] == numeros [i - 1]:
+                movimentos += 1
+        i -= 1
+    if movimentos != 0:
+        return True
+    else:
+        return False
 #colocar todas as funções
 #condição
 jogando = True
@@ -43,3 +84,7 @@ while jogando:
             print ('\033[1;32;40m{0}.'.format (cor), carta)
         elif extrai_naipe(carta) == '♠':
             print ('\033[1;34;40m{0}.'.format (cor), carta)
+        jogando = False
+        while len (baralho) > 1 and possui_movimentos_possiveis(baralho) != False:
+            escolha_carta = int (input ('\033[1;37;40mDigite o número da carta desejada: (1 a {0}):'.format (len(baralho))))
+        z = escolha_carta - 1
