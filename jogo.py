0672153da552471bb27713c4ffd1521d8729989d
ceciliaprado <ceciliadmp@al.insper.edu.br>
2021-05-10 13:49:22 -0300

    atualização

--- jogo.py
+++ jogo.py
@@ -27,6 +27,109 @@ def extrai_naipe (string):
         return '♣'
     else:
         return '♠'
+
+def extrai_valor (string):
+    if len (string) > 2:
+        return '10'
+    elif string [0] == 'A':
+        return 'A'
+    elif string [0] == '2':
+        return '2'
+    elif string [0] == '3':
+        return '3'
+    elif string [0] == '4':
+        return '4'
+    elif string [0] == '5':
+        return '5'
+    elif string [0] == '6':
+        return '6'
+    elif string [0] == '7':
+        return '7'
+    elif string [0] == '8':
+        return '8'
+    elif string [0] == '9':
+        return '9'
+    elif string [0] == '10':
+        return '10'
+    elif string [0] == 'J':
+        return 'J'
+    elif string [0] == 'Q':
+        return 'Q'
+    else:
+        return 'K'
+
+def possui_movimentos_possiveis (baralho):
+    naipes = []
+    numeros =[]
+    for carta in baralho:
+        if len(carta) == 2:
+            if carta [0] == 'J':
+                naipes.append (carta [1])
+                numeros.append ('11')
+            if carta [0] == 'Q':
+                naipes.append (carta [1])
+                numeros.append ('12')
+            if carta [0] == 'K':
+                naipes.append (carta [1])
+                numeros.append ('13')
+            if carta [0] == 'A':
+                naipes.append (carta [1])
+                numeros.append ('1')
+            if carta[0] != 'A' and carta[0] != 'J' and carta[0] != 'Q' and carta[0] != 'K':
+                naipes.append (carta[1])
+                numeros.append (carta [0])
+        else:
+            naipes.append  (carta [2])
+            numeros.append (10)
+    i = len(baralho) - 1
+    movimentos = 0
+    while i >= 1:
+        if i >= 4:
+            if naipes[i] == naipes [i - 3]:
+                movimentos += 1
+        if numeros [i] == numeros [i - 3]:
+            movimentos += 1
+        else:
+            if naipes [i] == naipes [i - 1]:
+                movimentos += 1
+            if numeros [i] == numeros [i - 1]:
+                movimentos += 1
+        i -= 1
+    if movimentos != 0:
+        return True
+    else:
+        return False
+
+def lista_movimentos_possiveis (baralho, carta_selecionada):
+    numero = []
+    naipe = []
+    for carta in baralho:
+        if len(carta) == 2:
+            numero.append (carta [0])
+            naipe.append (carta [1])
+        else:
+            numero.append (10)
+            naipe.append (carta [2])
+    movimentos = []
+    if carta_selecionada == 0:
+        return []
+    elif carta_selecionada >= 3:
+            if naipe[carta_selecionada] == naipe [carta_selecionada - 1 ] :
+                movimentos.append (1)
+            if naipe [carta_selecionada] == naipe [carta_selecionada - 3]:
+                movimentos.append (3)
+            if numero [carta_selecionada] == numero [carta_selecionada - 1]:
+                movimentos.append (1)
+            if numero [carta_selecionada] == numero [carta_selecionada -3]:     
+                movimentos.append (3)
+    else:
+        if naipe[carta_selecionada] == naipe [carta_selecionada - 1 ] :
+            movimentos.append (1)
+        if numero [carta_selecionada] == numero [carta_selecionada - 1]:
+            movimentos.append (1)
+    return movimentos      
+
+
 #colocar todas as funções
 #condição
 jogando = True
@@ -54,3 +157,5 @@ while jogando:
         baralho_escolhido = baralho [carta_selecionada]
         while possiveis == []:
             escolher_novamente = int (input('Essa carta não pode ser escolhida. Escolha outra: '))
+            possivel_2 = escolher_novamente - 1 
+            possiveis= lista_movimentos_possiveis
